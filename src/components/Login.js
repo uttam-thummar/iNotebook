7a0dca9310582c5391c725b86b6731a4e6759491
uttam-thummar <uttamthummar.mscit19@vnsgu.ac.in>
2022-01-13 17:53:33 +0530

    login-register functionality improved

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,7 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { useHistory } from 'react-router-dom';
+import "../css/login_register.css";
+import $ from "jquery";
 
 function Login(props) {
     const [Credentials, setCredentials] = useState({email: "", password: ""});
@@ -8,42 +10,113 @@ function Login(props) {
     const collectingCredentials = (e) => {
         setCredentials({...Credentials, [e.target.name]: e.target.value});
     };
-
     const login = async (e) => {
         e.preventDefault();
-        const apiCall = await fetch("http://localhost:5000/api/auth/login", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(Credentials)
-        });
-        const response = await apiCall.json();
-        if(response.success){
-            // save the token and redirect
-            localStorage.setItem('authToken', response.authToken);
-            props.configToast(response.message,'success');
-            history.push("/");
+        var emailRegx = /^([a-z0-9.-]+)@([a-z0-9-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+        var validStatus = true;
+        if(Credentials.email === ""){
+            $("#emailLabel").removeClass("focus-label");
+            invalidInputs("email", "Required!");
+            validStatus = false;
+        }else if(emailRegx.test(Credentials.email) === false){
+            $("#emailLabel").addClass("focus-label");
+            invalidInputs("email", "Enter valid email!");
+            validStatus = false;
         }else{
-            props.configToast(response.message,'danger');
+            validInputs("email")
+        }
+
+        if(Credentials.password === ""){
+            $("#passwordLabel").removeClass("focus-label");
+            invalidInputs("password", "Required!");
+            validStatus = false;
+        }else{
+            validInputs("password");
+        }
+
+        if(validStatus === true){
+            const apiCall = await fetch("http://localhost:5000/api/auth/login", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(Credentials)
+            });
+            const response = await apiCall.json();
+            if(response.success){
+                // save the token and redirect
+                localStorage.setItem('authToken', response.authToken);
+                props.configToast(response.message,'success');
+                history.push("/");
+            }else if(response.errors){
+                response.errors.forEach(error => {
+                    if(error.param === "email"){
+                        invalidInputs("email",error.msg);
+                    }else if(error.param === "password"){
+                        invalidInputs("password",error.msg);
+                    }
+                });
+            }else{
+                props.configToast(response.message,'danger');
+            }
+        }else{
+            props.configToast("Enter Valid Credentials.",'warning');
         }
     }
+
+    const invalidInputs = (field, message) => {
+        $(`#${field}Icon`).css("color", "#fd4444");
+        $(`#${field}Validation`).addClass("invalid-msg");
+        $(`#${field}Validation`).text(message);
+    }
+    const validInputs = (field) => {
+        $(`#${field}Label`).addClass("focus-label");
+        // $(`#${field}`).css("border-color", "#55d688");
+        $(`#${field}Icon`).css("color", "#55d688");
+        $(`#${field}Validation`).removeClass("invalid-msg");
+    }
+
+    useEffect(() => {
+        if(Credentials.email === ""){
+            $("#emailLabel").removeClass("focus-label");
+        }else{
+            $("#emailLabel").addClass("focus-label");
+        }
+        return () => {
+            //cleanup
+        }
+    }, [Credentials.email]);
+    useEffect(() => {
+        if(Credentials.password === ""){
+            $("#passwordLabel").removeClass("focus-label");
+        }else{
+            $("#passwordLabel").addClass("focus-label");
+        }
+        return () => {
+            //cleanup
+        }
+    }, [Credentials.password]);
 
     return (
         <>
             <div className='container mt-2'>
-                <h2>Login to continue to iNotebook</h2>
-                <form onSubmit={login}>
-                    <div className="mb-3">
-                        <label htmlFor="email" className="form-label">Email address</label>
-                        <input type="email" className="form-control" id="email" name='email' value={Credentials.email} aria-describedby="emailHelp" onChange={collectingCredentials}/>
-                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                <form onSubmit={login} className='box'>
+                    <h2 className='mb-5'>Login</h2>
+                    <div className='wrapper'>
+                        <input type="email" id="email" name='email' value={Credentials.email} onChange={collectingCredentials} spellCheck="false" required/>
+                        <div className='label' id='emailLabel'>Email Address</div>
+                        <div className='validation' id='emailValidation'></div>
+                        <span className='mdi mdi-email-outline icon' id='emailIcon'></span>
                     </div>
-                    <div className="mb-3">
-                        <label htmlFor="password" className="form-label">Password</label>
-                        <input type="password" className="form-control" id="password" name='password' value={Credentials.password} onChange={collectingCredentials}/>
+                    <div className='wrapper'>
+                        <input type="password" id="password" name='password' value={Credentials.password} onChange={collectingCredentials} required/>
+                        <div className='label' id='passwordLabel'>Password</div>
+                        <div className='validation' id='passwordValidation'></div>
+                        <span className='mdi mdi-key-outline icon' id='passwordIcon'></span>
                     </div>
-                    <button type="submit" className="btn btn-primary" id='liveToastBtn'>Login</button>
+                    <div>
+                        <button type="submit" className='btn-login' id='login'>Login</button>
+                    </div>
                 </form>
             </div>
         </>
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,7 @@
-import React, {useState} from 'react'
+import React, {useState,useEffect} from 'react'
 import { useHistory } from 'react-router-dom';
+import "../css/login_register.css";
+import $ from "jquery";
 
 function Register(props) {
     const [RegistrationDetails, setRegistrationDetails] = useState({name: "", email: "", password: ""});
@@ -8,45 +10,144 @@ function Register(props) {
     const collectingDetails = (e) => {
         setRegistrationDetails({...RegistrationDetails, [e.target.name]: e.target.value});
     };
-
     const register = async (e) => {
         e.preventDefault();
-        const apiCall = await fetch("http://localhost:5000/api/auth/register", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(RegistrationDetails)
-        });
-        const response = await apiCall.json();
-        if(response.success){
-            // save the token and redirect
-            localStorage.setItem('authToken', response.authToken);
-            history.push("/");
-            props.configToast(response.message,'success');
+        var emailRegx = /^([a-z0-9.-]+)@([a-z0-9-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+        var validStatus = true;
+
+        if(RegistrationDetails.name === ""){
+            $("#nameLabel").removeClass("focus-label");
+            invalidInputs("name", "Required!");
+            validStatus = false;
         }else{
-            props.configToast(response.message,'danger');
+            validInputs("name");
+        }
+
+        if(RegistrationDetails.email === ""){
+            $("#emailLabel").removeClass("focus-label");
+            invalidInputs("email", "Required!");
+            validStatus = false;
+        }else if(emailRegx.test(RegistrationDetails.email) === false){
+            $("#emailLabel").addClass("focus-label");
+            invalidInputs("email", "Enter valid email!");
+            validStatus = false;
+        }else{
+            validInputs("email")
+        }
+
+        if(RegistrationDetails.password === ""){
+            $("#passwordLabel").removeClass("focus-label");
+            invalidInputs("password", "Required!");
+            validStatus = false;
+        }else if(RegistrationDetails.password.length < 5 ){
+            $("#passwordLabel").addClass("focus-label");
+            invalidInputs("password", "Password must be atleast 5 characters long!");
+            validStatus = false;
+        }else{
+            validInputs("password");
+        }
+
+        if(validStatus === true){
+            const apiCall = await fetch("http://localhost:5000/api/auth/register", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(RegistrationDetails)
+            });
+            const response = await apiCall.json();
+            if(response.success){
+                // save the token and redirect
+                localStorage.setItem('authToken', response.authToken);
+                history.push("/");
+                props.configToast(response.message,'success');
+            }else if(response.errors){
+                response.errors.forEach(error => {
+                    if(error.param === "name"){
+                        invalidInputs("name",error.msg);
+                    }else if(error.param === "email"){
+                        invalidInputs("email",error.msg);
+                    }else if(error.param === "password"){
+                        invalidInputs("password",error.msg);
+                    }
+                });
+            }else{
+                props.configToast(response.message,'danger');
+            }
+        }else{
+            props.configToast("Enter Valid Details.",'warning');
         }
     }
+
+    const invalidInputs = (field, message) => {
+        $(`#${field}Icon`).css("color", "#fd4444");
+        $(`#${field}Validation`).addClass("invalid-msg");
+        $(`#${field}Validation`).text(message);
+    }
+    const validInputs = (field) => {
+        $(`#${field}Label`).addClass("focus-label");
+        // $(`#${field}`).css("border-color", "#55d688");
+        $(`#${field}Icon`).css("color", "#55d688");
+        $(`#${field}Validation`).removeClass("invalid-msg");
+    }
+
+    useEffect(() => {
+        if(RegistrationDetails.name === ""){
+            $("#nameLabel").removeClass("focus-label");
+        }else{
+            $("#nameLabel").addClass("focus-label");
+        }
+        return () => {
+            //cleanup
+        }
+    }, [RegistrationDetails.name]);
+    useEffect(() => {
+        if(RegistrationDetails.email === ""){
+            $("#emailLabel").removeClass("focus-label");
+        }else{
+            $("#emailLabel").addClass("focus-label");
+        }
+        return () => {
+            //cleanup
+        }
+    }, [RegistrationDetails.email]);
+    useEffect(() => {
+        if(RegistrationDetails.password === ""){
+            $("#passwordLabel").removeClass("focus-label");
+        }else{
+            $("#passwordLabel").addClass("focus-label");
+        }
+        return () => {
+            //cleanup
+        }
+    }, [RegistrationDetails.password]);
+
     return (
         <>
             <div className='container mt-2'>
-                <h2>Register to use iNotebook</h2>
-                <form onSubmit={register}>
-                    <div className="mb-3">
-                        <label htmlFor="name" className="form-label">Full Name</label>
-                        <input type="text" className="form-control" id="name" name="name" value={RegistrationDetails.name} onChange={collectingDetails}/>
+                <form onSubmit={register} className='box'>
+                    <h2 className='mb-5'>Register</h2>
+                    <div className='wrapper'>
+                        <input type="text" id="name" name='name' value={RegistrationDetails.name} onChange={collectingDetails} spellCheck="false" required/>
+                        <div className='label' id='nameLabel'>Full Name</div>
+                        <div className='validation' id='nameValidation'></div>
+                        <span className='mdi mdi-account-outline icon' id='nameIcon'></span>
                     </div>
-                    <div className="mb-3">
-                        <label htmlFor="email" className="form-label">Email address</label>
-                        <input type="email" className="form-control" id="email" name="email" value={RegistrationDetails.email} aria-describedby="emailHelp" onChange={collectingDetails}/>
-                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                    <div className='wrapper'>
+                        <input type="email" id="email" name='email' value={RegistrationDetails.email} onChange={collectingDetails} spellCheck="false" required/>
+                        <div className='label' id='emailLabel'>Email Address</div>
+                        <div className='validation' id='emailValidation'></div>
+                        <span className='mdi mdi-email-outline icon' id='emailIcon'></span>
                     </div>
-                    <div className="mb-3">
-                        <label htmlFor="password" className="form-label">Password</label>
-                        <input type="password" className="form-control" id="password" name="password" value={RegistrationDetails.password} onChange={collectingDetails}/>
+                    <div className='wrapper'>
+                        <input type="password" id="password" name='password' value={RegistrationDetails.password} onChange={collectingDetails} required/>
+                        <div className='label' id='passwordLabel'>Password</div>
+                        <div className='validation' id='passwordValidation'></div>
+                        <span className='mdi mdi-key-outline icon' id='passwordIcon'></span>
                     </div>
-                    <button type="submit" className="btn btn-primary">Register</button>
+                    <div>
+                        <button type="submit" className='btn-login' id='register'>Register</button>
+                    </div>
                 </form>
             </div>
         </>
